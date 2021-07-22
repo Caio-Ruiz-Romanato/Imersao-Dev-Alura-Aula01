42cf8f715b17426a7dd9d117fc7593e1f2653268
Caio Ruiz <68487921+Caio-Ruiz-Romanato@users.noreply.github.com>
2021-07-21 22:06:57 -0300

    Update scriptBTC.js

--- scriptBTC.js
+++ scriptBTC.js
@@ -1,5 +1,8 @@
+/* Variavel que obtem do usuario o valor convertendo o dado para número*/
 var BTC = Number(prompt("Digite quantos Bitcoin você quer trocar"))
 
+/* Variavel da operação matemática multiplicando */
 var real = BTC * 167879.84
 
+/* Emitindo o alerta com o resultado */ 
 alert(`O valor do Bitcoin convertido para real é ${real}`)
